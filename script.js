abaea7b67e237cd15401c57165bc2c93c1b47636
Meer Uzair <121447358+MeerUzairWasHere@users.noreply.github.com>
2023-02-06 14:55:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+const cards = document.querySelectorAll(".card");
+const resetBtn = document.querySelector(".resetGame");
+
+//variables
+var isFlipped = false;
+var firstCard;
+var secondCard;
+
+cards.forEach((card) => card.addEventListener("click", flip));
+
+function flip() {
+  //   console.log("Card flipped");
+  // console.log(this);
+  this.classList.add("flip");
+  if (!isFlipped) {
+    isFlipped = true;
+    firstCard = this;
+  } else {
+    secondCard = this;
+    console.log(firstCard);
+    console.log(secondCard);
+
+    checkIt();
+  }
+}
+
+function checkIt() {
+  //   console.log("Checking...");
+  if (firstCard.dataset.image === secondCard.dataset.image) {
+    success();
+  } else {
+    fail();
+  }
+}
+
+function success() {
+  //   console.log("Success");
+  firstCard.removeEventListener("click", flip);
+  secondCard.removeEventListener("click", flip);
+  reset();
+}
+
+function fail() {
+  //   console.log("Failed");
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    reset();
+  }, 1000);
+}
+
+function reset() {
+  isFlipped = false;
+  firstCard = null;
+  secondCard = null;
+}
+
+function shuffle() {
+  cards.forEach((card) => {
+    var index = Math.floor(Math.random() * 16);
+    card.style.order = index;
+  });
+}
+
+resetBtn.addEventListener("click", () => {
+  cards.forEach((card) => card.classList.remove("flip"));
+  shuffle();
+  cards.forEach((card) => card.addEventListener("click", flip));
+});
